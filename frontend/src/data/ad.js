2220a9836e8793c2786d1f44498430cc8878d446
CovetingAphid3 <tadiwachawanda@gmail.com>
2024-08-25 10:48:47 +0200

    ...

--- frontend/src/data/ad.js
+++ frontend/src/data/ad.js
@@ -2,20 +2,20 @@ const mockData = [
   {
     id: 1,
     imgUrl: '/assets/heart.svg',
-    name: 'Medical Support',
-    description: '24/7 Dedicated Support'
+    name: 'Name',
+    description: 'Description'
   },
   {
     id: 2,
     imgUrl: '/assets/payments.svg',
-    name: 'Secure Payments',
-    description: 'All Cards Accepted'
+    name: 'Name',
+    description: 'Description'
   },
   {
     id: 3,
     imgUrl: '/assets/delivery.svg',
-    name: 'Quality Assurance',
-    description: 'Blazingly Fast Service'
+    name: 'Template',
+    description: 'Description'
   }
 ];
 
--- frontend/src/data/bigAds.js
+++ frontend/src/data/bigAds.js
@@ -2,26 +2,26 @@ const bigAds = [
   {
     imageSrc: 'assets/writing.jpg',
     altText: 'Main Ad 1',
-    title: 'Prescription Services',
-    subtitle: 'Confidential and Expert Consultation'
+    title: 'Title',
+    subtitle: 'Subtitile'
   },
   {
     imageSrc: 'assets/blood.jpg',
     altText: 'Main Ad 2',
-    title: 'Health Check-ups',
-    subtitle: 'Comprehensive Assessments by Experts'
+    title: 'Title',
+    subtitle: 'Subtitle'
   },
   {
     imageSrc: 'assets/pharmacy3.png',
     altText: 'Main Ad 3',
-    title: 'Exclusive Offers',
-    subtitle: 'Discover Special Deals for Your Well-being'
+    title: 'Titile',
+    subtitle: 'Subtitile'
   },
   {
     imageSrc: 'assets/sale2.jpg',
     altText: 'Main Ad 4',
-    title: 'Flexible Payment Methods',
-    subtitle: 'Payment Options for Every Purchase'
+    title: 'Title',
+    subtitle: 'Subtititle'
   }
 ];
 
